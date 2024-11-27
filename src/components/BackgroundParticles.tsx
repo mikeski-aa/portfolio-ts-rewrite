33081982cdb2292846868a21587b394b885094e5
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-11-27 12:47:20 +0100

    Migration going OK so far

--- src/components/BackgroundParticles.tsx
+++ src/components/BackgroundParticles.tsx
@@ -1,27 +1,18 @@
 import { useCallback } from "react";
-import type { Container, Engine } from "tsparticles-engine";
+import type { Engine } from "tsparticles-engine";
 import Particles from "react-particles";
 
 import { loadSlim } from "tsparticles-slim";
 
 function BackgroundParticles() {
   const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-
     await loadSlim(engine);
   }, []);
 
-  const particlesLoaded = useCallback(
-    async (container: Container | undefined) => {
-      await console.log(container);
-    },
-    []
-  );
   return (
     <Particles
       id="tsparticles"
       init={particlesInit}
-      loaded={particlesLoaded}
       options={{
         background: {
           color: {
